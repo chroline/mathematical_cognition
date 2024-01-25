cf0c3de2c6f492b864b8d31ee3dc65d8ebdf63df
Cole Gawin <colegawin@gmail.com>
2024-01-25 15:13:25 -0800

    fix: import networks relative to file

--- program/networks.py
+++ program/networks.py
@@ -1,7 +1,13 @@
+from os import path
+
 import tensorflow as tf
 from autokeras import CUSTOM_OBJECTS
 
-digits_classifier = tf.keras.models.load_model('../trained_models/digits_classifier')
-addition_memory = tf.keras.models.load_model('../trained_models/addition_memory', custom_objects=CUSTOM_OBJECTS)
-multiplication_memory = tf.keras.models.load_model('../trained_models/multiplication_memory',
-                                                   custom_objects=CUSTOM_OBJECTS)
+DIRNAME = path.dirname(path.abspath(__file__))
+
+digits_classifier = tf.keras.models.load_model(path.join(DIRNAME, '../trained_models/digits_classifier'))
+addition_memory = tf.keras.models.load_model(path.join(DIRNAME, '../trained_models/addition_memory'),
+                                             custom_objects=CUSTOM_OBJECTS)
+multiplication_memory = tf.keras.models.load_model(
+    path.join(DIRNAME, '../trained_models/multiplication_memory'),
+    custom_objects=CUSTOM_OBJECTS)
